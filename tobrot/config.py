a0f1298753e61d75792d307992f981287d140e4a
darkhacker12121 <71131168+darkhacker12121@users.noreply.github.com>
2021-01-24 03:54:06 -0800

    Update config.py

--- tobrot/config.py
+++ tobrot/config.py
@@ -6,7 +6,7 @@ class Config(Config):
     TG_BOT_TOKEN= "1581011965:AAEy7O3dQwbgs-v5gvraHpQ5AAJ3GaVrH08" #imp
     APP_ID = 1733305 #imp
     API_HASH = "f423cffca6b5b7247b31b5b0df61f48d" #imp
-    AUTH_CHANNEL = [-1001377338298, 1156597097] #imp replace your_id with your id from telegram or delete
+    AUTH_CHANNEL = [-1001266398622, 1156597097] #imp replace your_id with your id from telegram or delete
     GLEECH_COMMAND = "gleech@torlechlkbot"
     YTDL_COMMAND = 'ytdl@torlechlkbot'
     TELEGRAM_LEECH_COMMAND_G = "tleech@torlechlkbot"
