17cc88d92bdd7fd91a621ce49038ac6e905d1035
darkhacker12121 <71131168+darkhacker12121@users.noreply.github.com>
2021-01-24 02:10:58 -0800

    Create config.py

---
+++ tobrot/config.py
@@ -0,0 +1,18 @@
+from tobrot.sample_config import Config
+
+#read readme too before filling these stuffs
+
+class Config(Config):
+    TG_BOT_TOKEN= "1581011965:AAEy7O3dQwbgs-v5gvraHpQ5AAJ3GaVrH08" #imp
+    APP_ID = 1733305 #imp
+    API_HASH = "f423cffca6b5b7247b31b5b0df61f48d" #imp
+    AUTH_CHANNEL = [-1001377338298, 1156597097] #imp replace your_id with your id from telegram or delete
+    INDEX_LINK = ""
+    GLEECH_COMMAND = "gleech@torlechlkbot"
+    YTDL_COMMAND = 'ytdl@torlechlkbot'
+    TELEGRAM_LEECH_COMMAND_G = "tleech@torlechlkbot"
+    CLONE_COMMAND_G = "gclone"
+    PYTDL_COMMAND_G = "pytdl"
+    DESTINATION_FOLDER = "Download"
+    LEECH_COMMAND = "leech@torlechlkbot"
+ 
