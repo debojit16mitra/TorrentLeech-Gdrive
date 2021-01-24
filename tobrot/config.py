4f8df6bc84aa33eeb411f1a682a97a6c3e4f3ca9
darkhacker12121 <71131168+darkhacker12121@users.noreply.github.com>
2021-01-24 02:45:02 -0800

    Update config.py

--- tobrot/config.py
+++ tobrot/config.py
@@ -14,4 +14,13 @@ class Config(Config):
     PYTDL_COMMAND_G = "pytdl"
     DESTINATION_FOLDER = "Download"
     LEECH_COMMAND = "leech@torlechlkbot"
- 
+    RCLONE_CONFIG = """
+[kmac]
+type = drive
+client_id = 24644445636-le8qtpevii1jf672k7seeg0u1ru5vp3j.apps.googleusercontent.com
+client_secret = 7129Wg4PYqfTgOqJZDVWyS2I
+scope = drive
+root_folder_id =
+token = {"access_token":"ya29.a0AfH6SMBGancUBWLmfF2MODJpNhyRg5hHM0JmzGaVf5kHlN_HGfisP-EYvQoyliKysbvKBvonmMeWy981Fb5C_cJXp7w0_L22OYrveuQg_rLyGWXzr1VQe05oHRee8UnmVyBijEvNJRkSTGYltOPeS_8EqZPZfOBQCk6obbwnbzA","token_type":"Bearer","refresh_token":"1//0g-ByVm7cD9_eCgYIARAAGBASNwF-L9IrnQVGb5UdfeMdrH1JjL93Z89sQwBtxlUKSk1eSuKQXcSxPfNRTegxif6jylpUyuh0dTg","expiry":"2021-01-22T09:38:19.9912246-08:00"}
+team_drive = 0ALyFD2RofrpdUk9PVA
+"""
