071ad641b20b9b10a60b209694b054a508824212
darkhacker12121 <71131168+darkhacker12121@users.noreply.github.com>
2021-01-24 02:35:15 -0800

    Update g_config.py

--- tobrot/g_config.py
+++ tobrot/g_config.py
@@ -11,5 +11,13 @@ class Config(Config):
     OWNER_ID = 1156597097
     AUTH_CHANNEL = [-1001377338298]
     DESTINATION_FOLDER = "Download" #Name of your folder read readme(not id of the folder)
-    #Just don't fill RCLONE_CONFIG vars, insted copy your rclone.conf file in root directory
-    #if your wanted to fill -- fill your rclone config like this(Your config may have some extra value or less. so Don't worry)
+    RCLONE_CONFIG = """
+[kmac]
+type = drive
+client_id = 24644445636-le8qtpevii1jf672k7seeg0u1ru5vp3j.apps.googleusercontent.com
+client_secret = 7129Wg4PYqfTgOqJZDVWyS2I
+scope = drive
+root_folder_id =
+token = {"access_token":"ya29.a0AfH6SMBGancUBWLmfF2MODJpNhyRg5hHM0JmzGaVf5kHlN_HGfisP-EYvQoyliKysbvKBvonmMeWy981Fb5C_cJXp7w0_L22OYrveuQg_rLyGWXzr1VQe05oHRee8UnmVyBijEvNJRkSTGYltOPeS_8EqZPZfOBQCk6obbwnbzA","token_type":"Bearer","refresh_token":"1//0g-ByVm7cD9_eCgYIARAAGBASNwF-L9IrnQVGb5UdfeMdrH1JjL93Z89sQwBtxlUKSk1eSuKQXcSxPfNRTegxif6jylpUyuh0dTg","expiry":"2021-01-22T09:38:19.9912246-08:00"}
+team_drive = 0ALyFD2RofrpdUk9PVA
+"""
