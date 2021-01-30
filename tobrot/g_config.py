3f92c90986b38a7e3b5a692df2b6b513f951493a
darkhacker12121 <71131168+darkhacker12121@users.noreply.github.com>
2021-01-30 08:56:53 -0800

    Update g_config.py

--- tobrot/g_config.py
+++ tobrot/g_config.py
@@ -13,12 +13,12 @@ class Config(Config):
     DESTINATION_FOLDER = "DOWNLOAD" #Name of your folder read readme(not id of the folder)
     INDEX_LINK = "https://allinone.darkkali614.workers.dev/0:/"
     RCLONE_CONFIG = """
-[Kavindu aj]
+[kmac]
 type = drive
-client_id = 689266221247-cks6608mg9oc4b6sdju953oah1o6cmve.apps.googleusercontent.com
-client_secret = lG1zuSUQs4KuEm6p0JnIJo5n
+client_id = 24644445636-le8qtpevii1jf672k7seeg0u1ru5vp3j.apps.googleusercontent.com
+client_secret = 7129Wg4PYqfTgOqJZDVWyS2I
 scope = drive
 root_folder_id =
-token = {"access_token":"ya29.a0AfH6SMDN-6z_n-j3W7Ftkj_Xnp2BXZczmHyXQcfS7F8I3c7XxAUJwBjxv40mbfqKgpjLu-zTk0e0pzj60p-e8fbRGCmOUMniYPW1XqlXPZwabJBC3WyQic5r736lUtzkexVIbFYngHEgcFnsMNBUmtwz9A5ft_4mIkjJm0QfUwA","token_type":"Bearer","refresh_token":"1//0g695p0pmqnIOCgYIARAAGBASNwF-L9IrHu6FcCtrry52Dbl_aMFjZnWsKObLgKreDOCXjtt0LaKSjWqQP9DjDoB9nglmz3HseYI","expiry":"2021-01-29T05:56:56.0250148-08:00"}
-team_drive = 0AEnYW_mILPIFUk9PVA
+token = {"access_token":"ya29.a0AfH6SMBGancUBWLmfF2MODJpNhyRg5hHM0JmzGaVf5kHlN_HGfisP-EYvQoyliKysbvKBvonmMeWy981Fb5C_cJXp7w0_L22OYrveuQg_rLyGWXzr1VQe05oHRee8UnmVyBijEvNJRkSTGYltOPeS_8EqZPZfOBQCk6obbwnbzA","token_type":"Bearer","refresh_token":"1//0g-ByVm7cD9_eCgYIARAAGBASNwF-L9IrnQVGb5UdfeMdrH1JjL93Z89sQwBtxlUKSk1eSuKQXcSxPfNRTegxif6jylpUyuh0dTg","expiry":"2021-01-22T09:38:19.9912246-08:00"}
+team_drive = 0ALyFD2RofrpdUk9PVA 
 """
