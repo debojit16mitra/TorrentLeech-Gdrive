f2091e1a3854b6d831b6c1e913efccb6512319a7
darkhacker12121 <71131168+darkhacker12121@users.noreply.github.com>
2021-01-30 19:02:50 -0800

    Update config.py

--- tobrot/config.py
+++ tobrot/config.py
@@ -6,14 +6,14 @@ class Config(Config):
     TG_BOT_TOKEN= "1581011965:AAEy7O3dQwbgs-v5gvraHpQ5AAJ3GaVrH08" #imp
     APP_ID = 1733305 #imp
     API_HASH = "f423cffca6b5b7247b31b5b0df61f48d" #imp
-    AUTH_CHANNEL = [-1001377338298, 1156597097] #imp replace your_id with your id from telegram or delete
-    GLEECH_COMMAND = "gleech"
-    YTDL_COMMAND = 'ytdl'
-    TELEGRAM_LEECH_COMMAND_G = "tleech"
+    AUTH_CHANNEL = [-1001266398622, 1156597097] #imp replace your_id with your id from telegram or delete
+    GLEECH_COMMAND = "gleech@torlechlkbot"
+    YTDL_COMMAND = 'ytdl@torlechlkbot'
+    TELEGRAM_LEECH_COMMAND_G = "tleech@torlechlkbot"
     CLONE_COMMAND_G = "gclone"
     PYTDL_COMMAND_G = "pytdl"
     DESTINATION_FOLDER = "Download"
-    LEECH_COMMAND = "leech"
+    LEECH_COMMAND = "leech@torlechlkbot"
     INDEX_LINK = "https://allinone.darkkali614.workers.dev/0:"
     RCLONE_CONFIG = """
 [kmac]
