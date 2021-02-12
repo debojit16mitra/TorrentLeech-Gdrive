bb6f3cf0cfcce01d5840140f2dc8e4a15a912914
debojit16mitra <33363581+debojit16mitra@users.noreply.github.com>
2021-02-12 11:02:54 +0530

    Update g_config.py

--- tobrot/g_config.py
+++ tobrot/g_config.py
@@ -5,20 +5,20 @@ from tobrot.sample_config import Config
 #FOR CUSTOM COMMANDS READ REAME AND FILL THEM...
 
 class Config(Config):
-    TG_BOT_TOKEN= "1581011965:AAEy7O3dQwbgs-v5gvraHpQ5AAJ3GaVrH08"
-    APP_ID = 1733305
-    API_HASH = "f423cffca6b5b7247b31b5b0df61f48d"
-    OWNER_ID = 1156597097
-    AUTH_CHANNEL = [-1001266398622]
+    TG_BOT_TOKEN= "1622073183:AAFEVcuHUpMQk7TzpcI1imadeq8YC4WyIY8"
+    APP_ID = 2776597
+    API_HASH = "2c0b3f7d9e283b9c6f5e60bcf063fdb4"
+    OWNER_ID = 998646192
+    AUTH_CHANNEL = [-1001305859685]
     DESTINATION_FOLDER = "DOWNLOAD" #Name of your folder read readme(not id of the folder)
-    INDEX_LINK = "https://allinone.darkkali614.workers.dev/0:/"
+    INDEX_LINK = "https://testing.bunny.workers.dev/0:"
     RCLONE_CONFIG = """
-[kmac]
+[BunnyDrive]
 type = drive
-client_id = 24644445636-le8qtpevii1jf672k7seeg0u1ru5vp3j.apps.googleusercontent.com
-client_secret = 7129Wg4PYqfTgOqJZDVWyS2I
+client_id = 1075453807926-4lo134sht0k6tkieusfcd73c0gk7jb3g.apps.googleusercontent.com
+client_secret = Oj8bcI-W6GR6xKQiWeaYi_q_
 scope = drive
-root_folder_id =
-token = {"access_token":"ya29.a0AfH6SMBGancUBWLmfF2MODJpNhyRg5hHM0JmzGaVf5kHlN_HGfisP-EYvQoyliKysbvKBvonmMeWy981Fb5C_cJXp7w0_L22OYrveuQg_rLyGWXzr1VQe05oHRee8UnmVyBijEvNJRkSTGYltOPeS_8EqZPZfOBQCk6obbwnbzA","token_type":"Bearer","refresh_token":"1//0g-ByVm7cD9_eCgYIARAAGBASNwF-L9IrnQVGb5UdfeMdrH1JjL93Z89sQwBtxlUKSk1eSuKQXcSxPfNRTegxif6jylpUyuh0dTg","expiry":"2021-01-22T09:38:19.9912246-08:00"}
-team_drive = 0ALyFD2RofrpdUk9PVA 
+root_folder_id = 
+token = {"access_token":"ya29.a0AfH6SMB6yHaTQ2nOOi7EW1QK4J734MCxdKoyspm-LZb1ri-1WqaiM0K75Dh2WJ3TOP5gdBOQyabhBfOSY3lB0oZYBUFzysVv5vlkbt7UR3cAcl-nzsF-kfshwpsj9KrXKmGz-iDzwKYB-Nlbhv3os84sH5l4MQMLCd4UjjYPLKk","token_type":"Bearer","refresh_token":"1//0gN38hjDQ_TMPCgYIARAAGBASNwF-L9IrTsoIIPsnV7PoiCNvxq4ndj5QsRFQbcZ231OUXH-thTEUj2agfTZug-4chDOpYudRNXI","expiry":"2021-02-11T11:40:00.9146405+05:30"}
+team_drive = 0AAoPf0BluNRqUk9PVA 
 """
